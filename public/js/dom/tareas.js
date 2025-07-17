5cf14c69e3c03cdc938a5cf61ff6211c82308589
diegohpezet <diegohpezet@gmail.com>
2025-07-17 12:14:44 -0300

    finish frontend

--- public/js/dom/tareas.js
+++ public/js/dom/tareas.js
@@ -1,6 +1,6 @@
 import { deleteTarea } from "../api/tareas.js";
 
-export async function render(tareas) {
+export async function renderList(tareas) {
   if (tareas.length === 0) {
     const root = document.querySelector('#root');
     root.innerHTML = '<p class="text-center">No hay tareas para mostrar</p>';
@@ -9,7 +9,7 @@ export async function render(tareas) {
 
   const root = document.querySelector('#root');
   root.innerHTML = '';  // Limpiamos el HTML
-  
+
   tareas.forEach((tarea) => {
     const card = document.createElement('div');
     card.classList.add('card', 'mb-3');
@@ -29,4 +29,24 @@ export async function render(tareas) {
     });
     root.appendChild(card);
   })
+}
+
+export async function renderSingle(tarea) {
+  const root = document.querySelector('#root');
+  root.innerHTML = `
+    <div class="">
+      <h1 class="card-title">Título de la tarea: ${tarea.titulo}</h1>
+      <p class="card-text">Descripción de la tarea: ${tarea.descripcion}</p>
+      <p class="card-text">La carpeta de entrega de la tarea: ${tarea.path}</p>
+    </div>
+    <div class="text-center">
+      <a href="./edit.html?id=${tarea.id}" class="btn btn-warning text-white">Editar</a>
+      <button id="btn-eliminar-${tarea.id}"class="btn btn-danger" data-id="${tarea.id}">Eliminar</button>
+    </div>
+  `
+  const button = root.querySelector(`#btn-eliminar-${tarea.id}`);
+  button.addEventListener('click', async () => {
+    await deleteTarea(button.dataset.id);
+    location.replace('./index.html');
+  });
 }
